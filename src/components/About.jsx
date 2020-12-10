fce6d6b33228f88ef3a4c296f2e67e58d756231b
peoray <peoray@yahoo.com>
2020-12-10 15:26:25 +0100

    add info about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,13 +7,13 @@ import SocialLinks from './SocialLinks';
 function About() {
   return (
     <div>
-      <div className='hero container-inner mx-auto flex flex-col sm:flex-row justify-between items-center pt-16 pb-8'>
-        <div className='font-bold lg:focus:w-full sm:w-3/5 text-center sm:text-left'>
+      <div className='flex flex-col items-center justify-between pt-16 pb-8 mx-auto hero container-inner sm:flex-row'>
+        <div className='font-bold text-center lg:focus:w-full sm:w-3/5 sm:text-left'>
           <div className='text-5xl leading-tight'>Emmanuel Raymond</div>
-          <div className='text-xl text-green-700 leading-tight'>
-            Software Engineer and Open Source Enthusiast
+          <div className='text-lg leading-tight text-green-700'>
+            Software Engineer, Technical Writer and Open Source Enthusiast
           </div>
-          <div className='mt-2 flex justify-center md:justify-start'>
+          <div className='flex justify-center mt-2 md:justify-start'>
             <SocialLinks />
           </div>
         </div>
@@ -22,13 +22,13 @@ function About() {
           <img
             src={author}
             alt='author'
-            className='mx-auto sm:mx-0 object-cover h-48 w-48 rounded-full'
+            className='object-cover w-48 h-48 mx-auto rounded-full sm:mx-0'
           />
         </div>
       </div>
       {/* <!-- end hero --> */}
 
-      <div className='container-inner mx-auto'>
+      <div className='mx-auto container-inner'>
         <div className='text-xl md:text-lg'>
           <p className=''>
             Hi 👋, I'm Emmanuel and welcome to my Digital Garden. I'm passionate
@@ -40,8 +40,7 @@ function About() {
             I’ve been doing software development for over 4+ years and enjoy
             working with technologies to build products. Currently, I work with
             Javascript and enjoy writing and teaching it through code and words.
-            I put great emphasis on code quality, testing and performance
-            throughout my projects.
+            I put great emphasis on code quality, user experience, developer experience, documentation, testing and performance throughout my projects.
           </p>
 
           <p className='mt-2'>
@@ -57,11 +56,11 @@ function About() {
           </p> */}
         </div>
 
-        <div className='flex justify-between items-center py-6'>
-          <div className='w-full sm:w-1/2 px-8 py-8 sm:py-0'>
+        <div className='flex items-center justify-between py-6'>
+          <div className='w-full px-8 py-8 sm:w-1/2 sm:py-0'>
             <img src={mind_map} alt='' />
           </div>
-          <div className='w-1/2 px-8 hidden sm:flex justify-end'>
+          <div className='justify-end hidden w-1/2 px-8 sm:flex'>
             <img src={code_review} alt='' />
           </div>
         </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,78 @@
-import React from "react"
-import SocialLinks from "./SocialLinks"
+import React from "react";
+import SocialLinks from "./SocialLinks";
 
 function Footer() {
   return (
-    <footer className="bg-green-700 text-white">
-      <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between py-8">
+    <footer className="text-white bg-green-700">
+      <div className="container flex flex-col items-center justify-between py-8 mx-auto lg:flex-row">
         <div className="mb-8 lg:mb-0">
-          <div>&copy; Emmanuel Raymond 2017 - {new Date().getFullYear()}. All rights reserved.</div>
+          <div className="text-sm">
+            <p className="text-white">
+              This website is{" "}
+              <a
+                href="https://github.com/peoray/peoray.dev"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                className="text-white hover:text-gray-300"
+              >
+                open source.
+              </a>{" "}
+              Proudly built with{" "}
+              <a
+                href="https://www.gatsbyjs.com/"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                className="text-white hover:text-gray-300"
+              >
+                Gatsby
+              </a>{" "}
+              and{" "}
+              <a
+                href="https://tailwindcss.com/"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                className="text-white hover:text-gray-300"
+              >
+                Tailwind CSS
+              </a>
+              , and hosted with
+              <a
+                href="https://www.netlify.com/"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                className="text-white hover:text-gray-300"
+              >
+                {" "}
+                Netlify.
+              </a>
+            </p>
+          </div>
+          <div className="flex items-center mt-2 lg:mt-0">
+            <p className="mr-2">
+              &copy; Emmanuel Raymond 2017 - {new Date().getFullYear()}. All
+              rights reserved.
+            </p>
+
+            {/* <svg
+              role="img"
+              aria-label="LGBTI Pride flag"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 777 480"
+              class="h-3"
+            >
+              <rect fill="#9d61a4" width="777" height="480"></rect>{" "}
+              <rect fill="#249cd5" width="777" height="400"></rect>{" "}
+              <rect fill="#70bc53" width="777" height="320"></rect>{" "}
+              <rect fill="#fdc753" width="777" height="240"></rect>{" "}
+              <rect fill="#f48d3a" width="777" height="160"></rect>{" "}
+              <rect fill="#d3555c" width="777" height="80"></rect>
+            </svg> */}
+          </div>
           {/* <div>
             Proudly powered by {' '}
             <a
               href="https://gatsby.org"
-              className="text-white hover:text-gray-400 font-normal underline"
+              className="font-normal text-white underline hover:text-gray-400"
               target="_blank"
               rel="nofollow noopener noreferrer"
             >
@@ -20,7 +81,7 @@ function Footer() {
             Deployed on {' '}
             <a
               href="https://www.netlify.com/"
-              className="text-white hover:text-gray-400 font-normal underline"
+              className="font-normal text-white underline hover:text-gray-400"
               target="_blank"
               rel="nofollow noopener noreferrer"
             >
@@ -30,24 +91,24 @@ function Footer() {
           <div>
             <a
               href="/rss.xml"
-              className="text-white hover:text-gray-400 font-normal underline"
+              className="font-normal text-white underline hover:text-gray-400"
             >
-              RSS Feed {' '}
+              RSS Feed{" "}
             </a>
-            | {' '}
+            |{" "}
             <a
               href="/sitemap.xml"
-              className="text-white hover:text-gray-400 font-normal underline"
+              className="font-normal text-white underline hover:text-gray-400"
             >
               Sitemap
             </a>
           </div>
         </div>
 
-          <SocialLinks />
+        <SocialLinks />
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
